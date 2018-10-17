3fbd5ead5e518ccc77dc4df07e4d720ff02a4747
Christoph Reif <christoph.reif@sendhybrid.com>
2018-10-17 10:05:08 +0200

    Refactor dl.go

--- dl.go
+++ dl.go
@@ -1,9 +1,21 @@
+/*
+
+https://readwrite.com/2013/10/02/github-for-beginners-part-2/
+
+> git remote add origin https://github.com/username/myproject.git
+> git remote -v
+
+git remote add origin https://github.com/MySheep/GoTools.git
+git push -u origin master
+
+*/
 package main
 
 import (
 	"fmt"
 	"log"
 	"os"
+	"runtime"
 	"strconv"
 	"strings"
 )
@@ -14,22 +26,6 @@ func getTs(i int) string {
 
 func getCurlCmd(i int) string {
 	return "curl " + getTs(i) + " --output " + strconv.Itoa(i) + ".ts"
-}
-
-/*
-see https://stackoverflow.com/questions/39868029/how-to-generate-a-sequence-of-numbers-in-golang
-
-a := makeRange(10, 20)
-fmt.Println(a)
-
-[10 11 12 13 14 15 16 17 18 19 20]
-*/
-func makeRange(min, max int) []int {
-	a := make([]int, max-min+1)
-	for i := range a {
-		a[i] = min + i
-	}
-	return a
 }
 
 func createTss(i, j int) []string {
@@ -66,10 +62,7 @@ func getJoinCmd(i, j int) string {
 	return "copy /b " + ts + " all.ts"
 }
 
-func main() {
-
-	i, err := strconv.Atoi(os.Args[1])
-	j, err := strconv.Atoi(os.Args[2])
+func dlScript(i, j int) {
 
 	fmt.Printf("Create dl.bat for i=%d, j=%d\n", i, j)
 
@@ -86,9 +79,56 @@ func main() {
 		fmt.Println(getCurlCmd(ii))
 		fmt.Fprintln(file, getCurlCmd(ii))
 	}
+}
 
-	// Create join tss .. "copy /b 1.ts+2.ts all.ts""
-	//
+func joinScript(i, j int) {
+
+	fmt.Printf("Create jn.bat for i=%d, j=%d\n", i, j)
+
+	// Open dl.bat file
+	file, err := os.Create("jn.bat")
+	if err != nil {
+		log.Fatal("Cannot create file", err)
+	}
+	defer file.Close()
+
 	fmt.Println(getJoinCmd(i, j))
 	fmt.Fprintln(file, getJoinCmd(i, j))
+}
+
+func removeScript(i, j int) {
+
+	fmt.Printf("Create rm.bat for i=%d, j=%d\n", i, j)
+
+	// Open dl.bat file
+	file, err := os.Create("rs.bat")
+	if err != nil {
+		log.Fatal("Cannot create file", err)
+	}
+	defer file.Close()
+
+	tss := createTss(i, j)
+
+	rmCmd := "rm"
+	if runtime.GOOS == "windows" {
+		rmCmd = "del"
+	}
+
+	for _, ts := range tss {
+		fmt.Fprintln(file, rmCmd+" "+ts)
+	}
+}
+
+func main() {
+
+	i, err := strconv.Atoi(os.Args[1])
+	j, err := strconv.Atoi(os.Args[2])
+
+	if err != nil {
+		log.Fatal("Cannot parse args", err)
+	}
+
+	dlScript(i, j)
+	joinScript(i, j)
+	removeScript(i, j)
 }
