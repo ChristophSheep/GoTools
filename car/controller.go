241835a5e8112206abd6623cf74d01d99630690c
Christoph Reif <christophreif@Christophs-MacBook-Pro.local>
2018-10-28 14:21:26 +0100

    create Home key for camera

--- car/controller.go
+++ car/controller.go
@@ -6,6 +6,13 @@ import (
 
 func keyCallback(w *glfw.Window, key glfw.Key, scancode int, action glfw.Action, mods glfw.ModifierKey) {
 
+	if key == glfw.KeyH {
+		scene.camera.scale = 0.02 // TODO Const
+		scene.camera.position[0] = 0.0
+		scene.camera.position[1] = 0.0
+		return
+	}
+
 	// ZOOM - in, out
 
 	//
